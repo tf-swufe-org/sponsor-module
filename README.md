d2a74b5c287b9417881e7f4d732272e064e23e1d
Tibor Bödecs <mail.tib@gmail.com>
2020-12-09 13:25:53 +0100

    Using the new install hook API

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Sponsor module for Feather CMS
 
 This module is reponsible for displaying a sponsor box for [Feather CMS](https://github.com/binarybirds/feather/).
+
+
+## Usage
+
+Simply inline the `Sponsor/Box` template anywhere in your template files.
+
+You can change the contents of the box through system variables using the admin interface.
--- Sources/SponsorModule/SponsorModule.swift
+++ Sources/SponsorModule/SponsorModule.swift
@@ -16,12 +16,67 @@ final class SponsorModule: ViperModule {
     }
 
     func boot(_ app: Application) throws {
-        app.hooks.register("installer", use: installerHook)
+        app.hooks.register("system-variables-install", use: systemVariablesInstallHook)
     }
 
     // MARK: - hooks
     
-    func installerHook(args: HookArguments) -> ViperInstaller {
-        SponsorInstaller()
+    func systemVariablesInstallHook(args: HookArguments) -> [[String: Any]] {
+        [
+            [
+                "key": "sponsor.isEnabled",
+                "name": "Sponsor box enabled",
+                "value": "true",
+                "note": "If enabled the box will appear when you inline the Sponsor/Box template",
+            ],
+            [
+                "key": "sponsor.title",
+                "name": "Sponsor box title",
+                "value": "Sponsor title",
+                "note": "Title of the sponsor box",
+            ],
+            [
+                "key": "sponsor.description",
+                "name": "Sponsor box description",
+                "value": "Sponsor description",
+                "note": "Used as a content for the sponsor box (can contain HTML text)",
+            ],
+            [
+                "key": "sponsor.image.alt",
+                "name": "Sponsor image alt text",
+                "value": "The.Swift.Dev.",
+                "note": "Alternative text for the sponsor image",
+            ],
+            [
+                "key": "sponsor.image.url",
+                "name": "Sponsor image URL",
+                "value": "https://theswiftdev.com/images/logos/logo.png",
+                "note": "Image URL for the sponsor box",
+            ],
+            [
+                "key": "sponsor.button.title",
+                "name": "Sponsor CTA button title",
+                "value": "The.Swift.Dev.",
+                "note": "CTA button title for the sponsor box",
+            ],
+            [
+                "key": "sponsor.button.url",
+                "name": "Sponsor CTA button url",
+                "value": "https://theswiftdev.com/",
+                "note": "CTA button link for the sponsor box",
+            ],
+            [
+                "key": "sponsor.more.title",
+                "name": "Sponsor more button title",
+                "value": "Learn more",
+                "note": "Learn more button title for the sponsor box",
+            ],
+            [
+                "key": "sponsor.more.url",
+                "name": "Sponsor more button url",
+                "value": "/about/",
+                "note": "Learn more button url for the sponsor box",
+            ],
+        ]
     }
 }
