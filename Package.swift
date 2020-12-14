553c62bf4036177777ffa8ee385f64e5ab97d1fd
Tibor Bödecs <mail.tib@gmail.com>
2020-12-14 21:35:28 +0100

    FC updates

--- Package.swift
+++ Package.swift
@@ -11,12 +11,23 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/binarybirds/feather-core", from: "1.0.0-beta"),
+        
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver", from: "4.0.0"),
+        .package(url: "https://github.com/binarybirds/liquid-local-driver", from: "1.2.0-beta"),
     ],
     targets: [
         .target(name: "SponsorModule", dependencies: [
             .product(name: "FeatherCore", package: "feather-core"),
         ], resources: [
             .copy("Bundle"),
+        ]),
+        .target(name: "Feather", dependencies: [
+            .product(name: "FeatherCore", package: "feather-core"),
+  
+            .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+            .product(name: "LiquidLocalDriver", package: "liquid-local-driver"),
+            
+            .target(name: "SponsorModule"),
         ]),
         .testTarget(name: "SponsorModuleTests",dependencies: [
             .target(name: "SponsorModule"),
